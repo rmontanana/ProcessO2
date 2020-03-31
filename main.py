5d85df798ff04357e8ecef1d62ec6455bc622d2b
Ricardo Montañana <rmontanana@gmail.com>
2020-03-31 20:06:13 +0200

    Add requirements, License, Readme and reorder main

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from ProcessO2 import ProcessO2
 
-model = ProcessO2('../MedicionesO2/*.csv', null_data=True)
 print("========= Files with duplicated data ===========")
 [print(i) for i in model.get_duplicated_files()]
+
+model = ProcessO2('../MedicionesO2/*.csv', null_data=True)
 data = model.get_all_data()
 print("================= With Null Data ===============")
 print(data.head())
 print(data.describe())
 print(data.dtypes)
-model = ProcessO2('../MedicionesO2/*.csv')
+
+model = ProcessO2('../O2Measures/*.csv')
 data = model.get_all_data()
 print("================ Without Null Data =============")
 print(data.head())
